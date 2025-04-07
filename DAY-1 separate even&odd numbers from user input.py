a3a9030f30d26b32e9f3c8004cce2bbd71ad3513
KOTESWARA RAO <krkoti100@gmail.com>
2025-04-07 18:02:06 +0530

    Update DAY-1 separate even&odd numbers from user input.py

--- DAY-1 separate even&odd numbers from user input.py
+++ DAY-1 separate even&odd numbers from user input.py
@@ -7,13 +7,17 @@ for i in a:
         even.append(i)
     else:
         odd.append(i)
-print("odd numbers:",*odd,sep=",")
-print("even numbers:",*even,sep=",")
+print("odd numbers:")
+print(*odd,sep=",")
+print("even numbers:")
+print(*even,sep=",")
 #output
 """
-enter numbers:1,2,3,4,5,6,7,8,9,10
-odd numbers:,1,3,5,7,9
-even numbers:,2,4,6,8,10
+enter numbers with comma separated:1,2,3,4,5,6,7,8,9,10
+odd numbers:
+1,3,5,7,9
+even numbers:
+2,4,6,8,10
 """
 
 
