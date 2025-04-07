a64680e2c150b9d568f99c8abcfd0688224b8c2f
KOTESWARA RAO <krkoti100@gmail.com>
2025-04-07 17:30:12 +0530

    Add files via upload

---
+++ DAY-1 separate even&odd numbers from user input.py
@@ -0,0 +1,22 @@
+#separating even and odd numbers from a user input.
+odd=[]
+even=[]
+a=tuple(map(int, input('enter numbers with comma separated:').split(",")))
+for i in a:
+    if i %2==0:
+        even.append(i)
+    else:
+        odd.append(i)
+print("odd numbers:",*odd,sep=",")
+print("even numbers:",*even,sep=",")
+#output
+"""
+enter numbers:1,2,3,4,5,6,7,8,9,10
+odd numbers:,1,3,5,7,9
+even numbers:,2,4,6,8,10
+"""
+
+
+
+
+    
